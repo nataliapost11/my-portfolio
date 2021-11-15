0618977dc66ba5c9eb09420b1a2f64432a489718
nataliapost11 <natalia.post11@gmail.com>
2021-11-15 03:21:44 -0500

    Updated about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../styles/About.css';
 import '../../styles/Section.css';
 
 export default function About() {
@@ -8,7 +9,17 @@ export default function About() {
         About Me
       </header>
       <div className="content">
-        About me content
+        <div className="profile-photo" ></div>
+        <p>
+          Hello, my name is Natalia Post, and I am a New York-based freelance Web Developer. If I am not freelancing at
+          some of New
+          York’s finest digital agencies, I am working on projects remotely on for clients all over the world. I have a
+          diverse set of skills, ranging from design to HTML + CSS + JavaScript.
+        </p><br />
+        <p>
+          Certificate of Completion the Institute For Robotic Process Automation & Artificial Intelligence Completed The
+          Next Gen Society: Intro to artificial intelligence.
+        </p>
       </div>
     </section>
   );
